025fe7a90e3c7e18e64a7ff1bcb47fde16a9e435
Mira Dayoub <miradayoub@Miras-MacBook-Pro.local>
2023-03-02 21:20:32 +0200

    lesson-01

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Mira Dayoub on 2/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ app-swoosh/app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,20 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Mira Dayoub on 2/27/23.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
